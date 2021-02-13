690af8a39e73fc43710f68c72de9327945a44f2c
Martin <martinagnalv@gmail.com>
2021-02-13 18:11:11 +0100

    stuff

---
+++ src/components/StartContainer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  Pressable
+} from 'react-native'
+
+const StartContainer = () => {
+  return (
+    <View>
+      <Text>STARTCONTAINER</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default StartContainer
