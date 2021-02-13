aacd03b04a1be9fbcf30cf5f0100bd62fbf00301
Martin <martinagnalv@gmail.com>
2021-02-13 18:06:15 +0100

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   extends: ['@react-native-community', 'react-app', 'react-app/jest'],
   rules: {
-    semi: ['error', 'never']
+    semi: ['error', 'never'],
+    'comma-dangle': ['warn', 'never']
   }
 }
